05ceabeafc2e79445e4ef49a29c87f3d0dd69d8c
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-09-13 13:48:29 -0600

    Update comfyinstall.sh

--- comfyinstall.sh
+++ comfyinstall.sh
@@ -6,3 +6,5 @@ git clone https://github.com/comfyanonymous/ComfyUI
 cd ComfyUI
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip3 install -r requirements.txt
+cd models/checkpoints
+wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt
