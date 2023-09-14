2bfeefc6e34ddf8ec83dc440ec0ba3befb469877
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-09-13 22:50:41 -0600

    Update comfyinstall.sh

--- comfyinstall.sh
+++ comfyinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python3 -m venv comfyui-venv
 source comfyui-venv/bin/activate
-cd comfyui-venv
 git clone https://github.com/comfyanonymous/ComfyUI
 cd ComfyUI
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
