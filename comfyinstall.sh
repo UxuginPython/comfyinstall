a2247134b3ce6ee31c4ea42750e6356151965b9d
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-09-13 22:46:09 -0600

    Update comfyinstall.sh

--- comfyinstall.sh
+++ comfyinstall.sh
@@ -8,3 +8,4 @@ pip3 install torch torchvision torchaudio --index-url https://download.pytorch.o
 pip3 install -r requirements.txt
 cd models/checkpoints
 wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt
+wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
