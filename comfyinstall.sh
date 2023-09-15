18daa8bb7168e4017eb65570ddd87edd5df883e8
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-09-15 17:32:17 -0600

    Update comfyinstall.sh

--- comfyinstall.sh
+++ comfyinstall.sh
@@ -5,9 +5,3 @@ git clone https://github.com/comfyanonymous/ComfyUI
 cd ComfyUI
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip3 install -r requirements.txt
-cd models/checkpoints
-wget https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-nonema-pruned.safetensors
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
-cd ../upscale_models
-wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth
